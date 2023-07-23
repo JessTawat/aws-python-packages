bc33b3b4414c477aab9a20accac958f6916c6243
Jess Tawat <jesstawat@gmail.com>
2023-07-23 19:14:42 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "aws-python-package",
+    version = "0.0.1",
+    author = "Jess Tawat",
+    author_email = "jess.tawat@gmail.com",
+    description = ("AWS Python Package"),
+    url = "https://github.com/JessTawat/aws-python-packages.git",
+    packages=[
+        'hello'
+    ],
+)
